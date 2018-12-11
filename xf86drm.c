a9d536438c5ad45440a0759c2c4098670af4975b
François Tigeot <ftigeot@wolfpond.org>
2018-12-11 23:04:01 +0100

    libdrm: Fix drmNodeIsDRM() on DragonFly
    * There is no way to check if a device name is really a drm device
      by looking it up in a virtual filesystem like on Linux
    
    * The major device number is also dynamically allocated from a pool,
      comparing it to a constant makes no sense
    
    * In the absence of better ideas, just assume the device name really
      points to a drm device and always return true
    
    Signed-off-by: François Tigeot <ftigeot@wolfpond.org>

--- xf86drm.c
+++ xf86drm.c
@@ -2796,6 +2796,8 @@ static bool drmNodeIsDRM(int maj, int min)
     snprintf(path, sizeof(path), "/sys/dev/char/%d:%d/device/drm",
              maj, min);
     return stat(path, &sbuf) == 0;
+#elif __DragonFly__
+    return true;	/* DragonFly BSD has no fixed major device numbers */
 #else
     return maj == DRM_MAJOR;
 #endif
